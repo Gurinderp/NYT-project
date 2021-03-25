abafa613f152699d2e512e40b9f7d46076e92622
rednirug <sgurinderpreet@gmail.com>
2021-03-25 00:50:05 -0700

    fixed failing db authentication and updated environment variables

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const env = process.env.DB_URI;
 
 mongoose
-	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.connect(env, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
 	.then((result) => console.log("Connected to DB"))
 	.catch((err) => console.log(err));
